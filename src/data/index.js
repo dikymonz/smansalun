fca68183f1c59b012f935a6a219cac16bf04cb7c
Dikymonz <diky.monz03@gmail.com>
2024-02-06 09:30:31 +0800

    require

--- src/data/index.js
+++ src/data/index.js
@@ -8,32 +8,39 @@ export const MyFeature = [
   {
     id: 1,
     image: StressIcon,
-    title: "Stress"
+    title: "Stress",
+    link: "/stress-page"
   },
   {
     id: 2,
     image: AnxietyIcon,
-    title: "Gangguan Kecemasan"
+    title: "Gangguan Kecemasan",
+    link: "/anxiety-page"
   },
   {
     id: 3,
     image: DepressionIcon,
-    title: "Depresi"
+    title: "Depresi",
+    link: "/depression-page"
   },
   {
     id: 4,
     image: TraumaIcon,
-    title: "Trauma"
+    title: "Chat AI",
+    link: "/chatai"
   },
-]
+];
+
 
 export const DokterUnggulan = [
   {
     id: 1,
     image: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yfGVufDB8fDB8fHww",
-    title: "Johri, S.Ag",
-    experience: "Kepala Sekolah",
+    title: "Dr. Dokteran",
+    experience: "5 Thn Pengalaman",
+    specialist: "Psikolog",
     alumni : "Universitas Brawijaya",
+    practice : "Los Angeles",
     rating: {
       stars: [
         { filled: true },
@@ -47,11 +54,13 @@ export const DokterUnggulan = [
     }
   },
   {
-    id: 1,
-    image: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yfGVufDB8fDB8fHww",
-    title: "H. Mustiadi, NH.",
-    experience: "Komite Sekolah",
-    alumni : "Universitas Brawijaya",
+    id: 2,
+    image: "https://images.unsplash.com/photo-1537368910025-700350fe46c7?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTB8fGRvY3RvcnxlbnwwfHwwfHx8MA%3D%3D",
+    title: "Dr. Stone",
+    experience: "7 Thn Pengalaman",
+    specialist: "Spesialis Otak",
+    alumni : "Institut Teknologi Bandung",
+    practice : "Gorontalo",
     rating: {
       stars: [
         { filled: true },
@@ -60,47 +69,30 @@ export const DokterUnggulan = [
         { filled: true },
         { filled: false }
       ],
-      average: 4.95,
+      average: 4.65,
       maxRating: 5
     }
   },
   {
-    id: 1,
-    image: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yfGVufDB8fDB8fHww",
-    title: "H. Martua Hamonongan Nasution, S.Pd.",
-    experience: "Pengawas Pembina",
-    alumni : "Universitas Brawijaya",
+    id: 3,
+    image: "https://images.unsplash.com/photo-1622253692010-333f2da6031d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGRvY3RvcnxlbnwwfHwwfHx8MA%3D%3D",
+    title: "Dr. Dicoding",
+    experience: "4 Thn Pengalaman",
+    specialist: "Spesialis bootcamp",
+    alumni : "Bina Nusantara",
+    practice : "Mataram",
     rating: {
       stars: [
         { filled: true },
         { filled: true },
         { filled: true },
-        { filled: true },
+        { filled: false },
         { filled: false }
       ],
-      average: 4.95,
+      average: 3.95,
       maxRating: 5
     }
   },
-  {
-    id: 1,
-    image: "https://images.unsplash.com/photo-1612349317150-e413f6a5b16d?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8ZG9jdG9yfGVufDB8fDB8fHww",
-    title: "Suhardi, A.Md,.",
-    experience: "Kepala Tata Usaha",
-    alumni : "Universitas Brawijaya",
-    rating: {
-      stars: [
-        { filled: true },
-        { filled: true },
-        { filled: true },
-        { filled: true },
-        { filled: false }
-      ],
-      average: 4.95,
-      maxRating: 5
-    }
-  },
-  
 ]
 
 
@@ -116,22 +108,22 @@ export const navLinks = [
   },
   {
     id: 2,
-    path: "articles",
+    path: "/articles",
     text: "Artikel",
   },
   {
     id: 3,
-    path: "doctor",
-    text: "Guru",
+    path: "/doctor",
+    text: "Dokter",
   },
   {
     id: 4,
-    path: "tentang",
+    path: "/About",
     text: "Tentang",
   },
   {
     id: 5,
-    path: "faq",
+    path: "/faq",
     text: "FAQ",
   },
 ]
@@ -139,12 +131,32 @@ export const navLinks = [
 export const faq = [
   {
     id: 1,
-    title: "Berapa jumlah guru yang ada di SMAN 1 Sembalun?",
+    title: "Apakah Boleh punya pacar 1?",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estlaborum.",
   },
   {
     id: 2,
-    title: "Kapan SMAN 1 Sembalun membuka pendaftraan siswa baru?",
+    title: "Apakah Boleh punya pacar 2?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estlaborum.",
+  },
+  {
+    id: 3,
+    title: "Apakah Boleh punya pacar 3?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estlaborum.",
+  },
+  {
+    id: 4,
+    title: "Apakah Boleh punya pacar 4?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estlaborum.",
+  },
+  {
+    id: 5,
+    title: "Apakah Boleh punya pacar 5?",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estlaborum.",
+  },
+  {
+    id: 6,
+    title: "Apakah Boleh punya pacar 6?",
     desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodoconsequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id estlaborum.",
   },
 ]
